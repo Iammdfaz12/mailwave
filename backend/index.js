3304464e7f7166b752514e7df8d6f859e893849c
Mohamed Fazil <alaudeenfazil2002@gmail.com>
2025-02-03 20:26:08 +0530

    CORS added

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,13 @@ require("dotenv").config();
 
 // Middlewares
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    methods: "GET, POST, PUT, DELETE",
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 // MongoDB Connection
